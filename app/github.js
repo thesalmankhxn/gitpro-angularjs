2b8ac3ccca3095cdb32bcd2da69fedbf889ca485
iamSK77 <sk77.mobilefix@gmail.com>
2020-10-10 15:27:07 +0530

    major improvements

---
+++ app/github.js
@@ -0,0 +1,14 @@
+(function(){
+    let github = function($http){
+
+        let getUser = function(username) {
+
+        };
+        
+        return {
+
+        };
+    };
+
+    let module = angular.module("gitPro");
+}());
