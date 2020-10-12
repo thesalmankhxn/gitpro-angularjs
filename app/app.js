65630e8152623dd7807bec32cbfb6f315a70a4de
iamSK77 <sk77.mobilefix@gmail.com>
2020-10-12 14:01:16 +0530

    major improvements

--- app/app.js
+++ app/app.js
@@ -1,7 +1,42 @@
-(function(){
+(function() {
     let app = angular.module("gitPro", []);
 
-    app.config(function() {
+    let MainController = function($scope, $http, $anchorScroll, $location) {
 
-    });
+        let onUserComplete = function(res) {
+            $scope.user = res.data;
+            $http.get($scope.user.repos_url)
+                .then(onRepos, onError)
+        };
+
+        let onRepos = function(res) {
+            $scope.repos = res.data;
+            $location.hash("userDetails");
+            $anchorScroll();
+        }
+
+        let onError = function(err) {
+            $scope.error = "Could not fetch the user";
+        };
+
+        let descendCountdown = function() {
+            $scope.countdown -= 1;
+            if($scope.countdown < 1) {
+                $scope.search($scope.username);
+            }
+        };
+
+        $scope.search = function(username) {
+            $http.get("http://api.github.com/users/" + username)
+            .then(onUserComplete, onError);
+        };
+
+        $scope.message = "Hello! Folks";
+        $scope.repoOrder = "-stargazers_count";
+        $scope.countdown = 5;
+
+    };
+
+    app.controller("MainController", ['$scope', '$http', '$anchorScroll', '$location', MainController]);
+
 }());
